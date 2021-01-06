70eaffebb1c21c3187f024aaa781100e799c5ac4
flatlandnick <nicholas.lunn@flatironschool.com>
2021-01-05 19:33:58 -0600

    cuisine model added

--- db/migrate/20201216160523_create_restaurants.rb
+++ db/migrate/20201216160523_create_restaurants.rb
@@ -2,7 +2,7 @@ class CreateRestaurants < ActiveRecord::Migration[6.1]
   def change
     create_table :restaurants do |t|
       t.string :name
-      t.string :cuisine
+      t.references :cuisine
       t.text :description
       t.integer :price
       t.integer :rating
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do 
+    Cuisine.create({
+        name: Faker::Restaurant.type
+    })
+end
 
 
-25.times do 
+50.times do 
     Restaurant.create({ 
         name: Faker::Restaurant.name, 
-        cuisine: Faker::Restaurant.type, 
+        cuisine_id: rand(1..10) , 
         description: Faker::Restaurant.description, 
         price: rand(1..4), 
         rating: rand(1..5), 

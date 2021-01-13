c8384a4814e9ce156789aec4661df7f683a286a5
flatlandnick <nicholas.lunn@flatironschool.com>
2021-01-13 11:07:38 -0600

    oauth

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_06_011636) do
+ActiveRecord::Schema.define(version: 2021_01_13_162922) do
 
   create_table "cuisines", force: :cascade do |t|
     t.string "name"
@@ -28,6 +28,16 @@ ActiveRecord::Schema.define(version: 2021_01_06_011636) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["cuisine_id"], name: "index_restaurants_on_cuisine_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+    t.string "uid"
+    t.string "provider"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
